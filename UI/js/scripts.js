19e15a83d9996b1f5e1a857d2e590b95c0428f4a
Emeryherve <52406178+Emeryherve@users.noreply.github.com>
2019-08-28 13:14:14 +0200

    Add admin can change user to a mentor (#5)
    * - Add platform homepage
    - Add navigation menu
    - Add platform logo
    - Add web responsiveness
    
    * - Add user signup form (#3)
    
    * - Add user signin (#4)
    
    * - Add admin can change user to a mentor
    - Add admin can view all users
    - Add admin can delete a user
    
    * - Add admin can change user to a mentor
    - Add admin can view all users
    - Add admin can delete a user

---
+++ UI/js/scripts.js
@@ -0,0 +1,81 @@
+
+const validate = () => {
+  let msg = '';
+  const fnames = document.querySelector('#fullname').value;
+  const uname = document.querySelector('#username').value;
+  const email = document.querySelector('#usermail').value;
+  const pswd = document.querySelector('#userpswd').value;
+  const repswd = document.querySelector('#confpswd').value;
+  // let exp =" /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/";
+  // var patt = new RegExp(exp);
+  if (fnames === '' || uname === '' || email === '' || pswd === '' || repswd === '') {
+    msg = 'Please fill out all required fields';
+
+    dialogbox(msg);
+  }
+};
+const check = () => {
+  let msg = '';
+  const email = document.querySelector('#ermail').value;
+  const pswd = document.querySelector('#pswd').value;
+  // let exp =" /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/";
+  // var patt = new RegExp(exp);
+  if (email === '' || pswd === '') {
+    msg = 'Please fill out all required fields';
+
+    dialogbox(msg);
+  }
+};
+
+let dialogbox = (message) => { // Get the modal
+  const modal = document.querySelector('#dialogbox');
+
+  const divMsg = document.querySelector('.dialog-content-js');
+
+
+  divMsg.textContent = message;
+  // Display the modal
+  modal.style.display = 'block';
+};
+// Function close the dialog box
+const closeDialog = () => {
+  document.querySelector('.modal').style.display = 'none';
+  const modal = document.querySelector('#dialogbox');
+  modal.style.display = 'none';
+};
+
+// Side Navigation
+const openNav = () => {
+  document.getElementById('mySidenav').style.width = '250px';
+};
+
+const navigate = () =>{
+  location.replace('./pages/signup.html');
+}
+
+const closeNav = () => {
+  document.getElementById('mySidenav').style.width = '0';
+}
+// close editing modal
+const closeModalEdit = () => {
+  document.querySelector('.modal-edit').style.display = 'none';
+};
+// view a trip
+const viewTrip = () => {
+  document.querySelector('.modal-view').style.display = 'block';
+};
+
+
+const closeModalView = () => {
+  document.querySelector('.modal-view').style.display = 'none';
+};
+const bookTrip = () => {
+
+};
+
+
+// view a user
+const viewUser = () => {
+  document.querySelector('.modal-view').style.display = 'block';
+};
+
