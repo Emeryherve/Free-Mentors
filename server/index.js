2ded9df93098efd0f6bdabe53106e00d5b03c048
Emeryherve <52406178+Emeryherve@users.noreply.github.com>
2019-08-31 14:14:17 +0200

    Ft user to mentor api 168190705 (#17)
    * - Add user signup api endpoint
    
    * - Add travis CI build status badge
    
    * Add coverage badge on README
    
    * Add coverage badge on README
    
    * - Add code climate mantainabilty badge
    
    * feat(user): user can signin
    
    * feat(user): user can signin
    
    * - Remove confidential files
    
    * - feat(admin): admin can change user to mentor
    
    * - feat(admin): admin can change user to mentor
    
    * - feat(admin): admin can change user to mentor

--- server/index.js
+++ server/index.js
@@ -4,12 +4,15 @@ import dotenv from 'dotenv'
 import bodyParse from 'body-parser';
 import userRoute from './routes/user_route';
 import sessionRoute from './routes/session_route';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../app.json';
 
 dotenv.config();
 const app = express();
 app.use(bodyParse.json());
 app.use('/api/v1', userRoute);
 app.use('/api/v1/sessions', sessionRoute);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 const port = process.env.PORT || 3000;
