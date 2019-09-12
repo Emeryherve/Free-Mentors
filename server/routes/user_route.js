34437dc525e51f8f1db770f525b791375993bcb2
Emery Herve KIREZI <emeryherve112@gmail.com>
2019-09-12 09:08:03 +0200

    Remove comments from user routes

--- server/routes/user_route.js
+++ server/routes/user_route.js
@@ -7,7 +7,4 @@ const router = express.Router();
 const { signUp, signIn } = UserController;
 router.post('/auth/signup', signUp);
 router.post('/auth/signin', signIn);
-// router.patch('/user/:userId', isUser, isAdmin, userController.changeUserToMentor);
-// router.get('/mentors', isUser, userController.getMentors);
-// router.get('/mentors/:mentorId', isUser, userController.getMentorById);
 export default router;
